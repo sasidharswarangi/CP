0524f3d47e511e6b465c1c40fc8cbfec38862c98
sasidhar.swarangi <sasidhar.swarangi@servicenow.com>
2023-02-10 11:28:46 +0530

    q45

---
+++ leetcode/q45.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int s= nums.size();
+
+        vector<int> val(s,s+1);
+        int ptr;
+        val[0]=0;
+        for(int i=0; i<s ; i++)
+        {
+            for (int j=i+1;(j<s)&&(j<=nums[i]+i);j++)
+                {
+                    if(val[j]>val[i]+1)
+                        val[j]=val[i]+1;
+                }
+        }
+
+        return val[s-1];
+    }
+};
